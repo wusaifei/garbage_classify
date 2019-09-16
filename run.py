a05607ad342fee2bd3e0805a0494c8dc04b738d6
仵赛飞 <1210063452@qq.com>
2019-09-16 19:47:55 +0800

    Update run.py

--- run.py
+++ run.py
@@ -4,23 +4,18 @@
 使用方法：
 （1）训练
 cd {run.py所在目录}
-python run.py --data_url='../datasets/garbage_classify/train_data' --train_url='../model_snapshots'
---num_classes=40 --deploy_script_path='./deploy_scripts' --test_data_url='../datasets/test_data'
---max_epochs=10
-
+python run.py --data_url='../datasets/garbage_classify/train_data' --train_url='./model_snapshots' --deploy_script_path='./deploy_scripts'
 （2）转pb
 cd {run.py所在目录}
-python run.py --mode=save_pb --deploy_script_path='./deploy_scripts'
---freeze_weights_file_path='../model_snapshots/weights_000_0.9811.h5' --num_classes=40
-
+python run.py --mode=save_pb --deploy_script_path='./deploy_scripts' --freeze_weights_file_path='../model_snapshots/weights_000_0.9811.h5' --num_classes=40
 （3）评价
 cd {run.py所在目录}
-python run.py --mode=eval --eval_pb_path='../model_snapshots/model' --num_classes=40
---test_data_url='../datasets/test_data'
+python run.py --mode=eval --eval_pb_path='../model_snapshots/model' --test_data_url='../datasets/garbage_classify/train_data'
 '''
 import os
+os.environ["CUDA_VISIBLE_DEVICES"] = "0,1"
 import tensorflow as tf
-from moxing.framework import file
+import shutil
 
 tf.app.flags.DEFINE_string('mode', 'train', 'optional: train, save_pb, eval')
 tf.app.flags.DEFINE_string('local_data_root', '/cache/',
@@ -35,7 +30,7 @@ tf.app.flags.DEFINE_integer('keep_weights_file_num', 20,
 tf.app.flags.DEFINE_integer('num_classes', 0, 'the num of classes which your task should classify')
 tf.app.flags.DEFINE_integer('input_size', 456, 'the input image size of the model')
 tf.app.flags.DEFINE_integer('batch_size', 8, '')
-tf.app.flags.DEFINE_float('learning_rate', 1e-4, '')
+tf.app.flags.DEFINE_float('learning_rate',1e-4, '')
 tf.app.flags.DEFINE_integer('max_epochs', 30, '')
 
 # params for save pb
@@ -69,37 +64,37 @@ def check_args(FLAGS):
     if FLAGS.mode == 'train':
         if FLAGS.data_url == '':
             raise Exception('you must specify FLAGS.data_url')
-        if not file.exists(FLAGS.data_url):
+        if not os.path.exists(FLAGS.data_url):
             raise Exception('FLAGS.data_url: %s is not exist' % FLAGS.data_url)
-        if FLAGS.restore_model_path != '' and (not file.exists(FLAGS.restore_model_path)):
+        if FLAGS.restore_model_path != '' and (not os.path.exists(FLAGS.restore_model_path)):
             raise Exception('FLAGS.restore_model_path: %s is not exist' % FLAGS.restore_model_path)
-        if file.is_directory(FLAGS.restore_model_path):
+        if os.path.isdir(FLAGS.restore_model_path):
             raise Exception('FLAGS.restore_model_path must be a file path, not a directory, %s' % FLAGS.restore_model_path)
         if FLAGS.train_url == '':
             raise Exception('you must specify FLAGS.train_url')
-        elif not file.exists(FLAGS.train_url):
-            file.make_dirs(FLAGS.train_url)
-        if FLAGS.deploy_script_path != '' and (not file.exists(FLAGS.deploy_script_path)):
+        elif not os.path.exists(FLAGS.train_url):
+            os.mkdir(FLAGS.train_url)
+        if FLAGS.deploy_script_path != '' and (not os.path.exists(FLAGS.deploy_script_path)):
             raise Exception('FLAGS.deploy_script_path: %s is not exist' % FLAGS.deploy_script_path)
-        if FLAGS.deploy_script_path != '' and file.exists(FLAGS.train_url + '/model'):
+        if FLAGS.deploy_script_path != '' and os.path.exists(FLAGS.train_url + '/model'):
             raise Exception(FLAGS.train_url +
                             '/model is already exist, only one model directoty is allowed to exist')
-        if FLAGS.test_data_url != '' and (not file.exists(FLAGS.test_data_url)):
+        if FLAGS.test_data_url != '' and (not os.path.exists(FLAGS.test_data_url)):
             raise Exception('FLAGS.test_data_url: %s is not exist' % FLAGS.test_data_url)
 
     if FLAGS.mode == 'save_pb':
         if FLAGS.deploy_script_path == '' or FLAGS.freeze_weights_file_path == '':
             raise Exception('you must specify FLAGS.deploy_script_path '
                             'and FLAGS.freeze_weights_file_path when you want to save pb')
-        if not file.exists(FLAGS.deploy_script_path):
+        if not os.path.exists(FLAGS.deploy_script_path):
             raise Exception('FLAGS.deploy_script_path: %s is not exist' % FLAGS.deploy_script_path)
-        if not file.is_directory(FLAGS.deploy_script_path):
+        if not os.path.isdir(FLAGS.deploy_script_path):
             raise Exception('FLAGS.deploy_script_path must be a directory, not a file path, %s' % FLAGS.deploy_script_path)
-        if not file.exists(FLAGS.freeze_weights_file_path):
+        if not os.path.exists(FLAGS.freeze_weights_file_path):
             raise Exception('FLAGS.freeze_weights_file_path: %s is not exist' % FLAGS.freeze_weights_file_path)
-        if file.is_directory(FLAGS.freeze_weights_file_path):
+        if os.path.isdir(FLAGS.freeze_weights_file_path):
             raise Exception('FLAGS.freeze_weights_file_path must be a file path, not a directory, %s ' % FLAGS.freeze_weights_file_path)
-        if file.exists(FLAGS.freeze_weights_file_path.rsplit('/', 1)[0] + '/model'):
+        if os.path.exists(FLAGS.freeze_weights_file_path.rsplit('/', 1)[0] + '/model'):
             raise Exception('a model directory is already exist in ' + FLAGS.freeze_weights_file_path.rsplit('/', 1)[0]
                             + ', please rename or remove the model directory ')
 
@@ -110,16 +105,16 @@ def check_args(FLAGS):
         if FLAGS.eval_weights_path != '' and FLAGS.eval_pb_path != '':
             raise Exception('you must specify only one of FLAGS.eval_weights_path '
                             'and FLAGS.eval_pb_path when you want to evaluate a model')
-        if FLAGS.eval_weights_path != '' and (not file.exists(FLAGS.eval_weights_path)):
+        if FLAGS.eval_weights_path != '' and (not os.path.exists(FLAGS.eval_weights_path)):
             raise Exception('FLAGS.eval_weights_path: %s is not exist' % FLAGS.eval_weights_path)
-        if FLAGS.eval_pb_path != '' and (not file.exists(FLAGS.eval_pb_path)):
+        if FLAGS.eval_pb_path != '' and (not os.path.exists(FLAGS.eval_pb_path)):
             raise Exception('FLAGS.eval_pb_path: %s is not exist' % FLAGS.eval_pb_path)
-        if not file.is_directory(FLAGS.eval_pb_path) or (not FLAGS.eval_pb_path.endswith('model')):
+        if not os.path.isdir(FLAGS.eval_pb_path) or (not FLAGS.eval_pb_path.endswith('model')):
             raise Exception('FLAGS.eval_pb_path must be a directory named model '
                             'which contain saved_model.pb and variables, %s' % FLAGS.eval_pb_path)
         if FLAGS.test_data_url == '':
             raise Exception('you must specify FLAGS.test_data_url when you want to evaluate a model')
-        if not file.exists(FLAGS.test_data_url):
+        if not os.path.exists(FLAGS.test_data_url):
             raise Exception('FLAGS.test_data_url: %s is not exist' % FLAGS.test_data_url)
 
 
@@ -132,29 +127,30 @@ def main(argv=None):
     else:
         FLAGS.data_local = os.path.join(FLAGS.local_data_root, 'train_data/')
         if not os.path.exists(FLAGS.data_local):
-            file.copy_parallel(FLAGS.data_url, FLAGS.data_local)
+            shutil.copytree(FLAGS.data_url, FLAGS.data_local)
         else:
             print('FLAGS.data_local: %s is already exist, skip copy' % FLAGS.data_local)
-
+        
     if not FLAGS.train_url.startswith('s3://'):
         FLAGS.train_local = FLAGS.train_url
     else:
         FLAGS.train_local = os.path.join(FLAGS.local_data_root, 'model_snapshots/')
         if not os.path.exists(FLAGS.train_local):
             os.mkdir(FLAGS.train_local)
-
+    
     if not FLAGS.test_data_url.startswith('s3://'):
         FLAGS.test_data_local = FLAGS.test_data_url
     else:
         FLAGS.test_data_local = os.path.join(FLAGS.local_data_root, 'test_data/')
         if not os.path.exists(FLAGS.test_data_local):
-            file.copy_parallel(FLAGS.test_data_url, FLAGS.test_data_local)
+            shutil.copytree(FLAGS.test_data_url, FLAGS.test_data_local)
         else:
             print('FLAGS.test_data_local: %s is already exist, skip copy' % FLAGS.test_data_local)
-
-    FLAGS.tmp = os.path.join(FLAGS.local_data_root, 'tmp/')
-    if not os.path.exists(FLAGS.tmp):
-        os.mkdir(FLAGS.tmp)
+    
+    # FLAGS.tmp = os.path.join(FLAGS.local_data_root, 'tmp/')
+    # print(FLAGS.tmp)
+    # if not os.path.exists(FLAGS.tmp):
+    #     os.mkdir(FLAGS.tmp)
 
     if FLAGS.mode == 'train':
         from train import train_model
