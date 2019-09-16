b2106db743bee22fb35c9ea93176d6ee8c1da0ce
仵赛飞 <1210063452@qq.com>
2019-09-16 16:19:15 +0800

    Update save_model.py

--- save_model.py
+++ save_model.py
@@ -3,8 +3,9 @@ import os
 
 import tensorflow as tf
 from keras import backend
-from keras.optimizers import adam,Nadam
-from moxing.framework import file
+from keras.optimizers import adam
+import shutil
+
 
 from train import model_fn
 
@@ -14,7 +15,7 @@ def load_weights(model, weighs_file_path):
         print('load weights from %s' % weighs_file_path)
         if weighs_file_path.startswith('s3://'):
             weighs_file_name = weighs_file_path.rsplit('/', 1)[1]
-            file.copy(weighs_file_path, '/cache/tmp/' + weighs_file_name)
+            shutil.copyfile(weighs_file_path, '/cache/tmp/' + weighs_file_name)
             weighs_file_path = '/cache/tmp/' + weighs_file_name
             model.load_weights(weighs_file_path)
             os.remove(weighs_file_path)
@@ -39,7 +40,7 @@ def save_pb_model(FLAGS, model):
             pb_save_dir_obs = pb_save_dir_local
 
     signature = tf.saved_model.signature_def_utils.predict_signature_def(
-        inputs={'input_img': model.input}, outputs={'output_score': model.output})
+        inputs={'input_img': model.input}, outputs={'output_score': model.layers.output})
     builder = tf.saved_model.builder.SavedModelBuilder(os.path.join(pb_save_dir_local, 'model'))
     legacy_init_op = tf.group(tf.tables_initializer(), name='legacy_init_op')
     builder.add_meta_graph_and_variables(
@@ -53,23 +54,22 @@ def save_pb_model(FLAGS, model):
     print('save pb to local path success')
 
     if pb_save_dir_obs.startswith('s3://'):
-        file.copy_parallel(os.path.join(pb_save_dir_local, 'model'),
+        shutil.copyfile(os.path.join(pb_save_dir_local, 'model'),
                                os.path.join(pb_save_dir_obs, 'model'))
         print('copy pb to %s success' % pb_save_dir_obs)
 
-    file.copy(os.path.join(FLAGS.deploy_script_path, 'config.json'),
+    shutil.copyfile(os.path.join(FLAGS.deploy_script_path, 'config.json'),
                   os.path.join(pb_save_dir_obs, 'model/config.json'))
-    file.copy(os.path.join(FLAGS.deploy_script_path, 'customize_service.py'),
+    shutil.copyfile(os.path.join(FLAGS.deploy_script_path, 'customize_service.py'),
                   os.path.join(pb_save_dir_obs, 'model/customize_service.py'))
-    if file.exists(os.path.join(pb_save_dir_obs, 'model/config.json')) and \
-            file.exists(os.path.join(pb_save_dir_obs, 'model/customize_service.py')):
+    if os.path.exists(os.path.join(pb_save_dir_obs, 'model/config.json')) and \
+            os.path.exists(os.path.join(pb_save_dir_obs, 'model/customize_service.py')):
         print('copy config.json and customize_service.py success')
     else:
         print('copy config.json and customize_service.py failed')
 
 
 def load_weights_save_pb(FLAGS):
-    # optimizer = adam(lr=FLAGS.learning_rate, clipnorm=0.001)
     optimizer = Nadam(lr=FLAGS.learning_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-08, schedule_decay=0.004)
     objective = 'categorical_crossentropy'
     metrics = ['accuracy']
