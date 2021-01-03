f27f3bbc8ad8437d4944ecbb0278dc1047a2a635
初识CV <wusaifei0922@gmail.com>
2021-01-03 11:23:15 +0800

    Update Save_path.py

--- Save_path.py
+++ Save_path.py
@@ -1,5 +1,5 @@
 import os
-dress="/media/vip/Data2/wusaifei/RSI_classification/pytorch/garbage_classify-master/datasets/garbage_classify/train_data/"
+dress="../garbage_classify-master/datasets/garbage_classify/train_data/"
 with open("train.txt","w") as f:
     for root,dirs,files in os.walk(dress):
         # root = root.replace(dress,'')
